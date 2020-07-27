64cd452eddb87b1ae89b452973dd80adfcdfc439
Valentine Samoylov <valentine.samoylov@gmail.com>
2020-07-27 07:25:34 +0300

    Some code changes and optimisations for mobile version.

--- #source/js/script.js
+++ #source/js/script.js
@@ -5,7 +5,7 @@
 		$("#js-menu__body-overlay").toggleClass("active");
 	});
 
-	$("#js-menu__item-link, #js-profile__btn, #js-menu__body-overlay").click(function (event) {
+	$(".menu__item-link, .profile__btn, #js-menu__body-overlay").click(function (event) {
 		$("#js-icon-menu, #js-menu-body, #js-menu__body-overlay").removeClass("active");
 	});
 	//</Icon menu button (burger menu)>============================================================
